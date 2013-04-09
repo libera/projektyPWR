af0950c955c13ac600a8ff778112af5e99069cd5
Michał Glenc <mglenc@gmail.com>
2013-04-09 12:59:06 +0200

    Kolejny update layoutu
    Wprowadzony kod odpowiedzialny za generowanie drzewa kursow z danych
    JSON oraz dodana strona logowania.

--- web/js/loader.js
+++ web/js/loader.js
@@ -1,15 +1,52 @@
+
 function resizeWindow() {
-	var viewportWidth = $(document).width();
 	//setting right-col width
-	$('.right-col').width(viewportWidth - $('.left-col').width() - 30);
+	$('.right-col').width($(document).width() - $('.left-col').width() - 50);
+	
+	//setting cols height
+	$('.left-col').height($(document).height() - 100);
+	$('.right-col').height($(document).height() - 100);
+}
+
+function showContent() {
+	$('#content').show();
+	
+	loadCourses();
+	
+	resizeWindow();
+}
+
+function hideContent() {
+	$('#content').hide();
+}
+
+function showLogin() {
+	$('#login').show();
+	$('#login').css('left', $(document).width()/2 - $('#login').width()/2);
+	$('#login').css('top', $(document).height()/2 - $('#login').height()/2-50);
+	
+	$('input#login-button').click(function() {
+		//TO DO: ajax login operation
+		hideLogin();
+		showContent();
+	});
+}
+
+function hideLogin() {
+	$('#login').hide();
 }
 
 $(document).ready(function() {
-	resizeWindow();
+	//when document is ready we display login window and hide content
+	hideLightbox();
+	hideContent();
+	showLogin();
 });
-
-
 
 $(window).resize(function() {
 	resizeWindow();
 });
+
+function hideLightbox() {
+	$('div#lightbox').hide();
+}
